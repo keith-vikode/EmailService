aa6be7ed93a777b7804cde52ac2086567a616afc
Keith Williams <kwilliams@sunbrandingsolutions.com>
2016-07-06 15:20:18 +0100

    Added a unit test project.

---
+++ test/EmailService.Core.Test/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmailService.Core.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void AssertTrue()
+        {
+            int expected = 4;
+            int actual = 2 + 2;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
