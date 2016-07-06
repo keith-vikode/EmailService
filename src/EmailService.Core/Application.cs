a6c49af2def19e2bc56d13c59993a8a385c19e21
Keith Williams <kwilliams@sunbrandingsolutions.com>
2016-07-06 14:59:41 +0100

    Added a basic class library to test the build.

---
+++ src/EmailService.Core/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService.Core
+{
+    public class Application
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public byte[] ConcurrencyToken { get; set; }
+    }
+}
---
+++ src/EmailService.Core/Translation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService.Core
+{
+    public class Translation
+    {
+        public CultureInfo Language { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
