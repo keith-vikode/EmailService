3495c3325c1088df09be9dec4da29f242031bfd4
Keith Williams <kwilliams@sunbrandingsolutions.com>
2016-08-08 17:12:13 +0100

    Gulp now minifies the CSS files.

--- src/EmailService.Web/gulpfile.js
+++ src/EmailService.Web/gulpfile.js
@@ -1,17 +1,30 @@
-﻿/*
+/// <binding AfterBuild='default' />
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+
+var paths = {
+    less:   './wwwroot/css/*.less',
+    css:    './wwwroot/css'
+};
 
 gulp.task('default', ['less'], function () {
     // place code for your default task here
 });
 
 gulp.task('less', function () {
-    return gulp.src('./wwwroot/css/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./wwwroot/css'));
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(gulp.dest(paths.css))
+        .pipe(cssnano())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.css));
 });
