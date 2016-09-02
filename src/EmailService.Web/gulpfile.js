449f057becb54f12928b42995eda82e8425e0d7f
Keith Williams <kwilliams@sunbrandingsolutions.com>
2016-09-02 16:05:23 +0100

    Included bower in the gulp script.

--- src/EmailService.Web/gulpfile.js
+++ src/EmailService.Web/gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
+var bower = require('gulp-bower');
 
 var paths = {
     less:   './wwwroot/css/*.less',
@@ -18,7 +19,11 @@ gulp.task('default', ['less'], function () {
     // place code for your default task here
 });
 
-gulp.task('less', function () {
+gulp.task('bower', function (cb) {
+    return bower(cb);
+});
+
+gulp.task('less', ['bower'], function () {
     return gulp.src(paths.less)
         .pipe(less())
         .pipe(gulp.dest(paths.css))
