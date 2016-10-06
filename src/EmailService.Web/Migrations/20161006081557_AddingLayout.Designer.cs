94d8ab4673b18e5cd05cefaaa4a3e36989c983f2
Keith Williams <keith@vikode.com>
2016-10-06 09:18:07 +0100

    Added migration for layouts

--- src/EmailService.Web/Migrations/20161006081557_AddingLayout.Designer.cs
+++ src/EmailService.Web/Migrations/20161006081557_AddingLayout.Designer.cs
@@ -8,9 +8,10 @@ using EmailService.Core.Entities;
 namespace EmailService.Web.Migrations
 {
     [DbContext(typeof(EmailServiceContext))]
-    partial class EmailServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20161006081557_AddingLayout")]
+    partial class AddingLayout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ src/EmailService.Web/Migrations/20161006081557_AddingLayout.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmailService.Web.Migrations
+{
+    public partial class AddingLayout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Layouts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ApplicationId = table.Column<Guid>(nullable: false),
+                    BodyHtml = table.Column<string>(nullable: true),
+                    ConcurrencyToken = table.Column<byte[]>(rowVersion: true, nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Layouts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Layouts_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LayoutId",
+                table: "Templates",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Layouts_ApplicationId",
+                table: "Layouts",
+                column: "ApplicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LayoutId",
+                table: "Templates");
+
+            migrationBuilder.DropTable(
+                name: "Layouts");
+        }
+    }
+}
