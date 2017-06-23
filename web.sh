57e99b6817deb33bb7cef872f74c21c6f5ac4289
Keith Williams <keith@vikode.com>
2017-06-23 23:29:57 +0100

    Web script now works on OSX.

--- web.sh
+++ web.sh
@@ -2,6 +2,6 @@
 export ASPNETCORE_ENVIRONMENT=Development
 
 pushd ./src/EmailService.Web
-dotnet watch run --server.urls http://localhost:2411;https://localhost:44320
+dotnet watch run --server.urls "http://localhost:2411;https://localhost:44320"
 cd
 exit /b
